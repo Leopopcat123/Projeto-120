50e9c45eb02e7d9dd99624109527e901abd3ae4e
Leopopcat123 <113531806+Leopopcat123@users.noreply.github.com>
2023-10-19 16:52:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 function setup() {
-  
+  canvas = createCanvas(300, 300);
+  canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+classifier = ml5.imageClassifier('MobileNet', modelLoaded)
 }
 
 function modelLoaded() {
-
+console.log("Model Loaded!");
 }
 
 function draw() {
- 
+image(video,0,  0,  300,  300);
+classifier.classify(video, gotResult);
+
 }
 var previousResult = '';
 
@@ -16,9 +22,12 @@ function gotResult(error, results) {
     console.error(error);
   } else {
     if((results[0].confidence > 0.5) && (previousResult != results[0].label)){
-      
-
-      
+     console.log(results);
+     previousResult = result[0].label;
+     var utterThis = new SpeechSynthesisUtterance(speakData);
+     stynth.speak(utterThis);
+     document.getElementById("resultObjectName").innerHTML = results[0].label;
+      document.getElementById("resultObjectAccuracy"),innerHTML = results[0].confidence.toFixed(3);
     }
   }
 }
